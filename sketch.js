c28397052074ff0bd48eedd8b11653b76f47d1ef
Champion176 <69591661+Champion176@users.noreply.github.com>
2021-05-28 19:02:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var formbg
+var form,pointerimg,pointer,gameState,circle1,circle2,circle3,circle4,circle1img
+var circles
+function preload()
+{  
+	formbg=loadImage ("js/download.jpg") 
+    pointerimg=loadImage("js/pointer.png")
+    circle1img=loadImage ("js/osu circle.jpg")
+    
+
+}
+
+function setup() {
+	createCanvas(1200, 700);
+form=new Form()
+game=new Game()
+game.play()
+//prompt("hi")
+
+
+}
+
+
+function draw() {
+ background("yellow")
+form.g1()
+ form.display()
+
+ if(gameState==1&&frameCount%4==0)
+ {
+     game.bg()
+ }
+    
+    
+    
+
+
+console.log(gameState)
+}
+
